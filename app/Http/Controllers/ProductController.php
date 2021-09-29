a1e636522b1dfdaeb50beaf51a4e9b0357271348
Maxim Zaboronkin <mvzaboronkin@gmail.com>
2021-09-30 00:00:46 +0300

    Описан метод create в ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -32,7 +33,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all()->sortBy('name');
+        return view('admin.productCreate', ['categories' => $categories]);
     }
 
     /**
